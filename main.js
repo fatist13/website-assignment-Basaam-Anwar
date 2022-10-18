b2f5203649947519b48555b3593604f588cc25a0
b.anwar <109045924+fatist13@users.noreply.github.com>
2022-10-18 14:48:23 -0600

    l

--- main.js
+++ main.js
@@ -1,43 +1,48 @@
 // JavaScript Code
 // Ze Event Listeners
-document.getElementById("btn1").addEventListener("click", btn1clicked);
-document.getElementById("btn2").addEventListener("click", btn2clicked);
-document.getElementById("btn3").addEventListener("click", btn3clicked);
-document.getElementById("btn4").addEventListener("click", btn4clicked);
-document.getElementById("a1");
-document.getElementById("a2");
-document.getElementById("a3");
-document.getElementById("a5");
-// Ze Functions
-function btn1clicked() {
+document.getElementById("check").addEventListener("click", getScore);
+
+// Ze functions
+function getScore() {
+    let numScore = 0;
+
+    //input
     let a1 = document.getElementById("a1").value.toLowerCase();
-    if (a1 === "the lands between") {
-        alert("Correct!")
-    } else {
-        alert("Incorrect")
-    }
-}
-function btn2clicked() {
     let a2 = document.getElementById("a2").value.toLowerCase();
-    if (a2 === "the demigods") {
-        alert("Correct!")
-    } else {
-        alert("Incorrect")
-    }
-}
-function btn3clicked() {
     let a3 = document.getElementById("a3").value.toLowerCase();
-    if (a3 === "queen marika" || a3 === "the demigods") {
-        alert("Correct!")
+    let a4 = document.getElementById("a4").value.toLowerCase();
+    //output
+    if (a1 === "the lands between") {
+        document.getElementById("a21").innerHTML = "correct";
+        numScore++;
     } else {
-        alert("Incorrect")
+        document.getElementById("a21").innerHTML = "incorrect";
+    }
+    if (a2 === "the demigods") {
+        document.getElementById("a22").innerHTML = "correct";
+        numScore++;
+    } else {
+        document.getElementById("a22").innerHTML = "incorrect";
+    }
+    if (a3 === "queen marika") {
+        document.getElementById("a23").innerHTML = "correct";
+        numScore++;
+    } else if (a2 === "the demigods") {
+        document.getElementById("a23").innerHTML = "correct";
+        numScore++;
+    } else {
+        document.getElementById("a23").innerHTML = "incorrect"
+    }
+    if (a4 === "guidance of grace") {
+        document.getElementById("a24").innerHTML = "correct";
+        numScore++;
+    } else {
+        document.getElementById("a24").innerHTML = "incorrect";
     }
 }
-function btn4clicked() {
-    let a4 = document.getElementById("a4").value.toLowerCase();
-    if (a4 === "guidance of grace" || a4 === "grace") {
-        alert("Correct!")
-    } else {
-        alert("Incorrect")
-    }
+
+// score
+
+if (numScore === 0) {
+    document.getElementById("score")
 }
