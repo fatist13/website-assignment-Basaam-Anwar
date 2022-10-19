011b6a54b0ef124ab1a29793c11a0d39a9f568bc
b.anwar <109045924+fatist13@users.noreply.github.com>
2022-10-19 14:46:28 -0600

    l

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // JavaScript Code
-// Ze Event Listeners
+// Ze Event Listener
 document.getElementById("check").addEventListener("click", getScore);
 
-// Ze functions
+// Ze function
 function getScore() {
     let numScore = 0;
 
@@ -13,31 +13,31 @@ function getScore() {
     let a4 = document.getElementById("a4").value.toLowerCase();
     //output
     if (a1 === "the lands between") {
-        document.getElementById("a21").innerHTML = ("CORRECT ");
+        document.getElementById("a21").innerHTML = (" CORRECT ");
         numScore++;
     } else {
-        document.getElementById("a21").innerHTML = ("INCORRECT ");
+        document.getElementById("a21").innerHTML = (" INCORRECT ");
     }
     if (a2 === "the demigods") {
-        document.getElementById("a22").innerHTML = ("CORRECT ");
+        document.getElementById("a22").innerHTML = (" CORRECT ");
         numScore++;
     } else {
         document.getElementById("a22").innerHTML = ("INCORRECT ");
     }
     if (a3 === "queen marika" || a3 === "the queen" || a3 === "ghost of queen elisabeth") {
-        document.getElementById("a23").innerHTML = ("CORRECT ");
+        document.getElementById("a23").innerHTML = (" CORRECT ");
         numScore++;
     } else if (a2 === "the demigods") {
-        document.getElementById("a23").innerHTML = ("CORRECT ");
+        document.getElementById("a23").innerHTML = (" CORRECT ");
         numScore++;
     } else {
-        document.getElementById("a23").innerHTML = ("INCORRECT ");
+        document.getElementById("a23").innerHTML = (" INCORRECT ");
     }
     if (a4 === "guidance of grace" || a4 === "grace") {
-        document.getElementById("a24").innerHTML = ("CORRECT ");
+        document.getElementById("a24").innerHTML = (" CORRECT ");
         numScore++;
     } else {
-        document.getElementById("a24").innerHTML = ("INCORRECT ");
+        document.getElementById("a24").innerHTML = (" INCORRECT ");
     }
     // score output
     if (numScore === 0) {
@@ -53,5 +53,3 @@ function getScore() {
         document.getElementById("scoreResult").innerHTML = ("4/4 (100%)");
     } 
 }
-
-
